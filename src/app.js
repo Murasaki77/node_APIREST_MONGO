f082c6011b9ad95dfbfbe61d2faeb4f42ad53aa6
Lopez <lopez_d77@hotmail.com>
2024-03-07 18:20:59 -0300

    Realizamos un servidor APIREST con Mongo y Express

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const {config} = require('dotenv')
+config()
+
+const bookRoutes = require('./routes/book.routes')
+
+
+//Usamos express para los middlewares
+const app = express()
+
+//Parseador de bodies
+app.use(bodyParser.json())
+
+//Aca conectamos la base de datos:
+mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DB_NAME})
+const db = mongoose.connection
+
+app.use('/books', bookRoutes)
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ( ) => {
+    console.log(`Servidor iniciado en el puerto ${port}`)
+})
